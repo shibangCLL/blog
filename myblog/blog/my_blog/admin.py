d8460a2946c31dd79aa6e927922e53ea485e132c
shibangCLL <869241891@qq.com>
2020-07-26 23:01:01 +0800

    完成详情页

--- myblog/blog/my_blog/admin.py
+++ myblog/blog/my_blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article,Category,Tag,Keyword,Carousel,AboutAuthor
+
+# 注册ArticlePost到admin中
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Keyword)
+admin.site.register(Carousel)
+admin.site.register(AboutAuthor)
--- myblog/blog/my_blog/views.py
+++ myblog/blog/my_blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from .models import Article, Carousel, AboutAuthor
+
+
 # Create your views here.
+# 视图函数
+def article_list(request):
+    articles = Article.objects.all()
+    carousels = Carousel.objects.all()
+    articles_top = Article.objects.all().filter(is_top=True)
+    authors = AboutAuthor.objects.all()
+
+    context = {'articles': articles, 'carousel': carousels, 'articles_top': articles_top, 'authors': authors}
+    return render(request, 'my_blog/index.html', context)
+
+
+def article_detail(request, id):
+    # 取出相应的文章
+    article = Article.objects.get(id=id)
+    # 需要传递给模板的对象
+    context = { 'article': article }
+    # 载入模板，并返回context对象
+    return render(request, 'my_blog/single-blog.html', context)
